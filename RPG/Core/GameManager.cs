aea67cc2d1267d9e027d664dff273ae3b52eaef8
sudo-YY <magicblacker6@gmail.com>
2025-11-17 16:23:30 +0800

    add story View

--- RPG/Core/GameManager.cs
+++ RPG/Core/GameManager.cs
@@ -11,6 +11,34 @@ namespace RPG.Core
         public static GameManager Instance => _instance ??= new GameManager();
 
         private ContentControl? _sceneHost;
+        public GameState CurrentState { get; private set; }
+
+        //private StoryManager _storyManager;
+        //private BattleManager _battleManager;
+
+        public void StartGame()
+        {
+            // 加载初始剧情
+            //var openingScenes = LoadOpeningStory();
+            //_storyManager = new StoryManager(openingScenes);
+            //_storyManager.OnStoryCompleted += OnOpeningStoryCompleted;
+            //_storyManager.StartStory();
+            CurrentState = GameState.Story;
+        }
+        private void OnOpeningStoryCompleted()
+        {
+            // 剧情结束，开始战斗
+            StartBattle();
+        }
+
+        private void StartBattle()
+        {
+            CurrentState = GameState.Battle;
+            //_battleManager = new BattleManager();
+            // 初始化战场角色...
+            // 监听战斗结束事件
+            // _battleManager.OnBattleCompleted += OnBattleCompleted;
+        }
 
         public void Initialize(ContentControl host)
         {
@@ -24,5 +52,12 @@ namespace RPG.Core
                 _sceneHost.Content = newScene;
             }
         }
+    }
+
+    public enum GameState
+    {
+        Story,
+        Battle,
+        Menu
     }
 }
